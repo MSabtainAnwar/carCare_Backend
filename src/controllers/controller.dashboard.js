540f1fb6d886424f3547271fc4f6fc74c555fccf
Muhammad Sabtain Anwar <71638141+MSabtainAnwar@users.noreply.github.com>
2023-04-26 08:07:00 +0500

    update

--- src/controllers/controller.dashboard.js
+++ src/controllers/controller.dashboard.js
@@ -23,8 +23,8 @@ const getDashboardCounts = async (req, res) => {
         },
       },
     ]);
-    // Total-Profit
-    let totalProfit = 0;
+    // Total-Sales
+    let totalSales = 0;
     // product-profit
     const productProfit = await ProductHistory.aggregate([
       {
@@ -34,8 +34,29 @@ const getDashboardCounts = async (req, res) => {
         },
       },
     ]);
-    totalProfit = productProfit?.[0]?.prodProfit || 0;
-    console.log("productProfit", totalProfit);
+    totalSales = productProfit?.[0]?.prodProfit || 0;
+    console.log("prod Profit", totalSales);
+    // Fetch-Services-price-in-order-table
+    const orderData = await Order.find().populate("servicesId");
+    let servicesAmount = 0,
+      otherServicesAmount = 0,
+      discountAmount = 0;
+    for (let i = 0; i < orderData.length; i++) {
+      discountAmount += orderData[i]?.discount || 0;
+      orderData[i].servicesId.forEach((service) => {
+        servicesAmount += service?.price || 0;
+      });
+      orderData[i].otherServices.forEach((service) => {
+        otherServicesAmount += service?.price || 0;
+      });
+    }
+    console.log("servicesAmount", servicesAmount);
+    console.log("discountAmount", discountAmount);
+    console.log("otherServices", otherServicesAmount);
+    totalSales += servicesAmount + otherServicesAmount - discountAmount;
+    let totalProfit = totalSales - expenseResult?.[0]?.totalExpense || 0;
+
+    console.log("totalSales", totalSales);
 
     // create-final-object
     const analyticsObj = {
@@ -45,6 +66,8 @@ const getDashboardCounts = async (req, res) => {
       totalProducts,
       totalExpense: expenseResult?.[0]?.totalExpense || 0,
       totalReminders: totalReminders?.length || 0,
+      totalSale: totalSales,
+      totalProfit,
     };
     res.status(200).json(responseStatus(true, "ok", "Success", analyticsObj));
   } catch (error) {
